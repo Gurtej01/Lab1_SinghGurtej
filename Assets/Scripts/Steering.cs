db0c64cf7a8d176db0c3fb8595f2a5b112db2684
Gurtej01 <Gurtej.Singh@georgebrown.ca>
2025-01-25 22:25:00 -0700

    Added basic return SeekForce functionality inside Steering Static class

--- Assets/Scripts/Steering.cs
+++ Assets/Scripts/Steering.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 public static class Steering
 {
     // "Seek is a static method that can be applied to any GameObject" -- Done for you here
-    public static Vector2 Seek(Rigidbody2D seeker, Vector2 target, float moveSpeed, float turnSpeed)
+    public static Vector2 Seek(Rigidbody2D seeker, Vector3 target, float moveSpeed, float turnSpeed)
     {
+        Vector2 currentVelocity = seeker.linearVelocity;
+        Vector2 desiredVelocity = ( target - seeker.transform.position).normalized * moveSpeed;
+        Vector2 seekForce = desiredVelocity - currentVelocity;
+        return seekForce;
         // 2% -- Output seeking force (smooth acceleration leading to curved motion)
         // 2% -- Use seeker.MoveRotation to rotate the seeker towards its velocity
         // (Instead of rotating towards the mouse cursor, you need to rotate it towards its velocity direction)
-        return Vector2.zero;
+        // return Vector2.zero;
     }
 }
